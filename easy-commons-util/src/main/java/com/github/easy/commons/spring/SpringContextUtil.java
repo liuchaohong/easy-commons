a8ae03c50af4ce4163b158eca8348757ebc45746
liuchaohong <470585213@qq.com>
2015-12-19 20:41:27 +0800

    1

--- easy-commons-util/src/main/java/com/github/easy/commons/spring/SpringContextUtil.java
+++ easy-commons-util/src/main/java/com/github/easy/commons/spring/SpringContextUtil.java
@@ -1,26 +1,26 @@
 package com.github.easy.commons.spring;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
  * Spring Context工具类
  * @author LIUCHAOHONG
  *
  */
-public class SpringContextUtil {
+public class SpringContextUtil implements ApplicationContextAware{
 
 	private static ApplicationContext context = null;
 
-	public static ApplicationContext getContext() {
-		return context;
-	}
-
-	public static void setContext(ApplicationContext context) {
-		SpringContextUtil.context = context;
-	}
-
 	public static Object getBean(String beanId) {
 		return context.getBean(beanId);
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		this.context = applicationContext;
 	}
 	
 }

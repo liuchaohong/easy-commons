17ed13b7ebe5acc3327c7a892fb3ca5fd206580e
liuchaohong <470585213@qq.com>
2015-12-23 17:21:07 +0800

    1

--- easy-commons-util/src/main/java/com/github/easy/commons/spring/ApplicationContextHolder.java
+++ easy-commons-util/src/main/java/com/github/easy/commons/spring/ApplicationContextHolder.java
@@ -3,6 +3,7 @@ package com.github.easy.commons.spring;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
@@ -35,6 +36,25 @@ public class ApplicationContextHolder implements ApplicationContextAware{
 		return getApplicationContext().getBean(beanName);
 	}
 	
+	public static <T> T getBean(Class<T> clazz) {
+	    T beanInstance;
+	    try {
+	      beanInstance = applicationContext.getBean(clazz);
+	    } catch (NoSuchBeanDefinitionException e) {
+	    	logger.info(String.format("Bean '{}' not be found", clazz));
+	      return null;
+	    }
+	    return beanInstance;
+	}
+
+	public static <T> T getBean(String beanName, Class<T> clazz) {
+		Object beanInstance = getBean(beanName);
+		if (beanInstance == null) {
+			return null;
+		}
+		return clazz.cast(beanInstance);
+	}
+		  
 	public static void cleanHolder() {
 		applicationContext = null;
 	}
